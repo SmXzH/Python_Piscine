c177759eefb11de433ad5c1cc832e34816b21374
SmXzH <zhakypov1488@gmail.com>
2024-06-07 18:29:28 +0400

    chore: Refactor OOP code and add new character classes

---
+++ 03-OOP/ex00/S1E9.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+
+class Character(ABC):
+    """
+    Abstract base class representing a character.
+    """
+
+    def __init__(self, first_name, is_alive=True):
+        """
+        Constructor for the Character class.
+
+        :param first_name: First name of the character.
+        :param is_alive: Boolean indicating if the character is alive. Default is True.
+        """
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+    @abstractmethod
+    def die(self):
+        """
+        Abstract method to change the health state of the character to dead.
+        """
+        ...
+
+class Stark(Character):
+    """
+    Class representing a Stark character, inheriting from Character.
+    """
+
+    def __init__(self, first_name, is_alive=True):
+        """
+        Constructor for the Stark class.
+
+        :param first_name: First name of the Stark character.
+        :param is_alive: Boolean indicating if the Stark character is alive. Default is True.
+        """
+        self.family_name = first_name
+        self.is_alive = is_alive
+
+    def die(self):
+        """
+        Method to change the health state of the Stark character to dead.
+        """
+        self.is_alive = False
---
+++ 03-OOP/ex01/S1E7.py
@@ -0,0 +1,9 @@
+from S1E9 import Character
+
+class Baratheon(Character):
+
+    ...
+class Lannister(Character):
+    ...
+    def create_lannister(name):
+        ...
